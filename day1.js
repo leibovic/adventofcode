a3793aa86c9fa945d4c405f6a1e0ad8fc9906cfd
Margaret Leibovic <mleibovic@league.com>
2018-12-24 14:04:29 -0500

    Part 2 of Day 1

--- day1.js
+++ day1.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
  
 fs.readFile('inputs/day1.txt', 'utf8', (err, contents) => {
-    var values = contents.split('\n');
-    var reduced = values.reduce((accumulator, currentValue) => {
-        if (!currentValue) {
-            return accumulator;
-        }
-        return accumulator + parseInt(currentValue);
-    }, 0);
-    console.log(reduced);
+    var values = contents.split('\n').filter(value => value.length > 0);
+    findFrequencySeenTwice(values);
 });
+
+function reduce(values, frequency) {
+    return values.reduce((accumulator, currentValue) => {
+        return accumulator + parseInt(currentValue);
+    }, frequency);
+}
+
+function findFrequencySeenTwice(values) {
+    var seen = {};
+    var seenTwice = undefined;
+    var frequency = 0;
+
+    while (seenTwice === undefined) {
+        frequency = values.reduce((accumulator, currentValue) => {
+            if (!seen[accumulator]) {
+                seen[accumulator] = true;
+            } else if (seenTwice === undefined) {
+                seenTwice = accumulator;
+            }
+            return accumulator + parseInt(currentValue);
+        }, frequency);
+    }
+    console.log("First frequency seen twice: " + seenTwice);
+}
