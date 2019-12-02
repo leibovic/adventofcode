3ba808cba625d86973d07b809847091d5a694fd9
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-12-02 16:46:34 -0500

    day 2 part 2

--- 2019/day2.js
+++ 2019/day2.js
@@ -2,9 +2,25 @@ const fs = require('fs');
  
 fs.readFile('inputs/day2.txt', 'utf8', (err, contents) => {
     const values = contents.split(',').map(value => parseInt(value));
-    runIntcodeProgram(values);
-    console.log(values.join(','));
+    const { noun, verb } = findOutput(19690720, values);
+    console.log(noun, verb);
 });
+
+const findOutput = (desiredOutput, values) => {
+    for (let noun = 0; noun < 100; noun++) {
+        for (let verb = 0; verb < 100; verb++) {
+            let valuesCopy = [...values];
+            valuesCopy[1] = noun;
+            valuesCopy[2] = verb;
+            runIntcodeProgram(valuesCopy);
+            if (valuesCopy[0] === desiredOutput) {
+                return { noun, verb };
+            }
+        }
+    }
+
+    return { noun: -1, verb: -1 };
+}
 
 const runIntcodeProgram = (values) => {
     let currentPosition = 0;
