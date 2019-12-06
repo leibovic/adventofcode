aed8e337c964567ab741fed38dcbcdeb1589416d
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-12-06 10:30:59 -0500

    day 6 part 2

--- 2019/day6.js
+++ 2019/day6.js
@@ -18,7 +18,8 @@ fs.readFile('inputs/day6.txt', 'utf8', (err, contents) => {
         totalOrbits += sumOrbits(node);
     }
 
-    console.log(totalOrbits);
+    console.log("total orbits:", totalOrbits);
+    console.log("min transfers:", minTransfers("YOU", "SAN"));
 });
 
 const sumOrbits = (node) => {
@@ -26,4 +27,24 @@ const sumOrbits = (node) => {
         return 0;
     }
     return 1 + sumOrbits(orbits[node]);
+}
+
+const makePath = (node) => {
+    if (!orbits[node]) {
+        return [];
+    }
+    return [node].concat(makePath(orbits[node]));
+}
+
+const minTransfers = (node1, node2) => {
+    const path1 = makePath(node1);
+    const path2 = makePath(node2);
+
+    for (let i = 0; i < path1.length; i++) {
+        for (let j = 0; j < path2.length; j++) {
+            if (path1[i] === path2[j]) {
+                return i + j - 2; // subtract 2 because we don't want to include original nodes in count
+            }
+        }
+    }
 }
