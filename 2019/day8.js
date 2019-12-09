cd5c34a697f506a3660bc7c734c2aa5ab1edce86
Margaret Leibovic <margaret.leibovic@gmail.com>
2019-12-08 21:10:15 -0500

    day 8 part 2

--- 2019/day8.js
+++ 2019/day8.js
@@ -4,28 +4,41 @@ fs.readFile('inputs/day8.txt', 'utf8', (err, data) => {
     const width = 25;
     const height = 6;
     const layerSize = width * height;
-    const layers = [];
+    
+    const layerPixelCounts = [];
+    const image = [];
 
     for (let i = 0; i < data.length; i++) {
-        if (i % layerSize === 0) {
-            layers.push({});
+        const pixelIndex = i % layerSize;
+        if (pixelIndex === 0) {
+            layerPixelCounts.push({});
         }
 
         const pixel = data[i];
-        const pixelCount = layers[layers.length - 1][pixel];
-        layers[layers.length - 1][pixel] = pixelCount ? pixelCount + 1 : 1;
+        if (!image[pixelIndex] || image[pixelIndex] === "2") {
+            image[pixelIndex] = pixel;
+        }
+
+        const layerIndex = layerPixelCounts.length - 1;
+        const pixelCount = layerPixelCounts[layerIndex][pixel];
+        layerPixelCounts[layerIndex][pixel] = pixelCount ? pixelCount + 1 : 1;
     }
 
     let fewestZeroCount = Infinity;
     let fewestZeroIndex;
-    for (let i = 0; i < layers.length; i++) {
-        let zeroCount = layers[i]["0"] ? layers[i]["0"] : 0;
+    for (let i = 0; i < layerPixelCounts.length; i++) {
+        let zeroCount = layerPixelCounts[i]["0"] ? layerPixelCounts[i]["0"] : 0;
         if (zeroCount < fewestZeroCount) {
             fewestZeroCount = zeroCount;
             fewestZeroIndex = i;
         }
     }
 
-    const fewestZeroLayer = layers[fewestZeroIndex];
+    const fewestZeroLayer = layerPixelCounts[fewestZeroIndex];
     console.log(fewestZeroLayer["1"] * fewestZeroLayer["2"]);
+
+    let imageString = image.join("").replace(/0/g, "■").replace(/1/g, "□");
+    for (let i = 0; i < imageString.length; i = i + width) {
+        console.log(imageString.substring(i, i + width))
+    }
 ;});
