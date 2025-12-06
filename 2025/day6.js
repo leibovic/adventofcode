1e82399d19a1cd9a4bbff396866b130f1b15f17a
Margaret Leibovic <margaret.leibovic@gmail.com>
2025-12-06 06:29:01 -0500

    part 2

--- 2025/day6.js
+++ 2025/day6.js
@@ -1,33 +1,73 @@
 const fs = require("fs");
 fs.readFile("inputs/day6.txt", "utf8", (err, contents) => {
-  const lines = contents
-    .split("\n")
-    .filter((value) => value.length > 0)
-    .map((line) => line.split(" ").filter((word) => word.length > 0));
-  console.log(lines);
+  const lines = contents.split("\n").filter((value) => value.length > 0);
+  const grid = lines.map((line) => line.split(""));
 
-  const columns = lines[0].length;
+  const numRows = grid.length;
+  const numCols = grid[0].length;
+
   const results = [];
-  for (let i = 0; i < columns; i++) {
-    const operator = lines[lines.length - 1][i];
-    console.log(operator);
+  let currentProblemOperator = null;
+  let problemNumbers = [];
 
-    let result = operator === "*" ? 1 : 0;
+  // Process columns right-to-left
+  for (let col = numCols - 1; col >= 0; col--) {
+    // Check if column is empty (all spaces)
+    const isEmpty = grid.every((row) => !row[col] || row[col] === " ");
 
-    for (let j = 0; j < lines.length - 1; j++) {
-      const value = parseInt(lines[j][i], 10);
-      console.log("  " + value);
+    if (isEmpty) {
+      // This is a separator - finalize current problem if any
+      if (problemNumbers.length > 0) {
+        let result = problemNumbers[0];
+        for (let i = 1; i < problemNumbers.length; i++) {
+          if (currentProblemOperator === "+") {
+            result += problemNumbers[i];
+          } else {
+            result *= problemNumbers[i];
+          }
+        }
+        results.push(result);
+        problemNumbers = [];
+        currentProblemOperator = null;
+      }
+      continue;
+    }
 
-      if (operator === "+") {
-        result += value;
+    // Build number from this column (top to bottom, excluding operator row)
+    let numberStr = "";
+    for (let row = 0; row < numRows - 1; row++) {
+      const char = grid[row][col];
+      if (char !== " ") {
+        numberStr += char;
+      }
+    }
+
+    // Get operator from bottom
+    const operator = grid[numRows - 1][col];
+    if (operator && operator !== " ") {
+      currentProblemOperator = operator;
+    }
+
+    if (numberStr) {
+      const number = parseInt(numberStr, 10);
+      problemNumbers.unshift(number); // Add to front (reading right-to-left)
+    }
+  }
+
+  // Finalize last problem
+  if (problemNumbers.length > 0) {
+    let result = problemNumbers[0];
+    for (let i = 1; i < problemNumbers.length; i++) {
+      if (currentProblemOperator === "+") {
+        result += problemNumbers[i];
       } else {
-        result *= value;
+        result *= problemNumbers[i];
       }
     }
     results.push(result);
   }
-  console.log("Results: " + results);
 
+  console.log("Results: " + results);
   const total = results.reduce((acc, curr) => acc + curr, 0);
   console.log("Total: " + total);
 });
